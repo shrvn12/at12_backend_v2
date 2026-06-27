78b1ca748beb45c511e6df6f097404e52f4b180b
shrvn12 <singhshravan1208@gmail.com>
2026-06-27 14:58:44 +0530

    FIX: resolved dependencies issues

--- middlewares/verifyPassword.js
+++ middlewares/verifyPassword.js
@@ -1,5 +1,5 @@
 const userModel = require("../models/user.model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const verifyPassword = async (req, res, next) => {
     const data = req.body;
